34064c74013ce9af993da3d49674f133c1dc231f
AlanQ03 <alanq3333@gmail.com>
2026-04-08 19:19:50 -0400

    Organized folders, fixed homepage, modified blackjack.py refs and command.txt

--- casino/views/blackjack.py
+++ casino/views/blackjack.py
@@ -14,7 +14,7 @@ def play_blackjack(request):
             dealer_hand = game.dealer_hand[0] # Show only one dealer card
             if player_hand == 21:
                 Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance + bet * 1.5)
-                return render(request, 'casino/templates/blackjack.html', {'message': 'Blackjack! You win!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
+                return render(request, 'casino/templates/pages/blackjack.html', {'message': 'Blackjack! You win!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
             elif player_hand < 21:
                 input = request.POST.get('action')
                 while input == 'hit':
@@ -22,15 +22,15 @@ def play_blackjack(request):
                     player_hand = game.calculate_hand_value(game.player_hand)
                     if player_hand > 21:
                         Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance - bet)
-                        return render(request, 'casino/templates/blackjack.html', {'message': 'Bust! You lose.', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
+                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'Bust! You lose.', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
                     input = request.POST.get('action')
                     game.dealer_play()
                     dealer_hand = game.calculate_hand_value(game.dealer_hand)
                     if dealer_hand > 21 or player_hand > dealer_hand:
                         Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance + bet)
-                        return render(request, 'casino/templates/blackjack.html', {'message': 'You win!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
+                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'You win!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
                     elif player_hand < dealer_hand:
                         Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance - bet)
-                        return render(request, 'casino/templates/blackjack.html', {'message': 'You lose.', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
+                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'You lose.', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
                     else:
-                        return render(request, 'casino/templates/blackjack.html', {'message': 'Push!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
+                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'Push!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
