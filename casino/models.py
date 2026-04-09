65b6f1d79780244821b0e3e6ca9a6ad29d0c177d
Ryan Szczesny <ryanszczesny@yahoo.com>
2026-04-09 13:09:47 -0400

    Fixed blackjack logic, added new model for GameSessions, ran migrations, added models to admin.py

--- casino/models.py
+++ casino/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,3 +7,19 @@ class Users(models.Model):
     name = models.CharField(max_length=20)
     balance = models.IntegerField(default=1000)
 
+class GameSession(models.Model):
+    GAME_CHOICES = [
+        ('blackjack', 'Blackjack'),
+        ('roulette', 'Roulette'),
+        ('slots', 'Slots'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.CharField(max_length=20, choices=GAME_CHOICES)
+    bet_amount = models.IntegerField()
+    result = models.CharField(max_length=10) #win or loss
+    payout = models.IntegerField() #change in balance after game
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.game} - {self.result} - {self.payout}"
--- casino/views/blackjack.py
+++ casino/views/blackjack.py
@@ -1,36 +1,70 @@
 from django.shortcuts import render
 from game_logic.blackjack import Blackjack
-from models import Users
+from models import Users, GameSession
 
 def play_blackjack(request):
+    user = Users.objects.get(id=request.user.id)
+
     if request.method == 'POST':
         bet = int(request.POST.get('bet'))
-        if bet > Users.objects.get(id=request.user.id).balance:
-            return render(request, 'blackjack.html', {'error': 'Insufficient balance'})
+
+        if bet > user.balance:
+            return render(request, 'casino/templates/pages/blackjack.html', {'error': 'Insufficient balance'})
+
+        game = Blackjack()
+        game.deal_initial_cards()
+
+        player_hand = game.calculate_hand_value(game.player_hand)
+        dealer_hand = game.dealer_hand[0]
+
+        if player_hand == 21:
+            user.balance += int(bet * 1.5)
+            result = 'win'
+
         else:
-            game = Blackjack()
-            game.deal_initial_cards()
-            player_hand = game.calculate_hand_value(game.player_hand)
-            dealer_hand = game.dealer_hand[0] # Show only one dealer card
-            if player_hand == 21:
-                Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance + bet * 1.5)
-                return render(request, 'casino/templates/pages/blackjack.html', {'message': 'Blackjack! You win!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
-            elif player_hand < 21:
-                input = request.POST.get('action')
-                while input == 'hit':
-                    game.player_hit()
-                    player_hand = game.calculate_hand_value(game.player_hand)
-                    if player_hand > 21:
-                        Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance - bet)
-                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'Bust! You lose.', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
-                    input = request.POST.get('action')
-                    game.dealer_play()
-                    dealer_hand = game.calculate_hand_value(game.dealer_hand)
-                    if dealer_hand > 21 or player_hand > dealer_hand:
-                        Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance + bet)
-                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'You win!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
-                    elif player_hand < dealer_hand:
-                        Users.objects.filter(id=request.user.id).update(balance=Users.objects.get(id=request.user.id).balance - bet)
-                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'You lose.', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
-                    else:
-                        return render(request, 'casino/templates/pages/blackjack.html', {'message': 'Push!', 'player_hand': player_hand, 'dealer_hand': dealer_hand, 'balance': Users.objects.get(id=request.user.id).balance})
+            action = request.POST.get('action')
+
+            if action == 'hit':
+                game.player_hit()
+                player_hand = game.calculate_hand_value(game.player_hand)
+
+                if player_hand > 21:
+                    user.balance -= bet
+                    result = 'loss'
+                else:
+                    return render(request, 'casino/templates/pages/blackjack.html', {
+                        'player_hand': player_hand,
+                        'dealer_hand': dealer_hand
+                    })
+
+            elif action == 'stand':
+                game.dealer_play()
+                dealer_hand = game.calculate_hand_value(game.dealer_hand)
+
+                if dealer_hand > 21 or player_hand > dealer_hand:
+                    user.balance += bet
+                    result = 'win'
+                elif player_hand < dealer_hand:
+                    user.balance -= bet
+                    result = 'loss'
+                else:
+                    result = 'push'
+
+        user.save()
+
+        GameSession.objects.create(
+            user=request.user,
+            game='blackjack',
+            bet_amount=bet,
+            result=result,
+            payout=bet if result == 'win' else -bet if result == 'loss' else 0
+        )
+
+        return render(request, 'casino/templates/pages/blackjack.html', {
+            'result': result,
+            'player_hand': player_hand,
+            'dealer_hand': dealer_hand,
+            'balance': user.balance
+        })
+
+    return render(request, 'casino/templates/pages/blackjack.html')
